1b8cee0b0091dde506100c0ac1737f7ca57bac72
Devin Halladay <devin@devinhalladay.com>
2020-12-02 23:27:39 -0500

    temperature converter working. did more clojure learning

--- reagent-cljs/src/cljs/guis/temperature-converter.cljs
+++ reagent-cljs/src/cljs/guis/temperature-converter.cljs
@@ -1,58 +1,59 @@
 (ns guis.temperature-converter
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :refer [atom]]))
 
-(def temperature-data (r/atom {:celcius 0 :fahrenheit 0}))
-
-(defn calc-temp [deg unit]
-  (cond-> 0
-    (= unit "fahrenheit") (* (- deg 32) (/ 5 9))
-    (= unit "celsius") (+ 32 (* deg (/ 9 5)))))
-
-(defn input-element
-  "An input element which updates its value on change"
-  [id name type value]
-  [:input {:id id
-           :name name
-           :class "form-control"
-           :type type
-           :defaultValue @value
-           :on-change #(swap! temperature-data (keyword type) (calc-temp (-> % .-target .-value) @type))
-           ;; Used swap! beacuse intuitively I guess that sounds like it's for
-           ;; transient changes vs !reset for, uh, resetting state
-  }])
-
-(defn fahrenheit-input
-  [deg]
-  (input-element "fahrenheit" "fahrenheit" "number" deg))
-
-(defn celsius-input
-  [deg]
-  (input-element "celsius" "celsius" "number" deg))
+(def celsius->fahrenheit #(+ 32 (* (/ 9 5) %)))
+(def fahrenheit->celsius #(* (/ 5 9) (- % 32)))
 
 (defn component []
-  [:div
-   [:h1 "Temperature Converter"]
-   (let [f (atom 0)]
-     (fahrenheit-input f))
-   (let [c (atom 0)]
-     (celsius-input c))])
+  (let [temperature (atom {:celsius "" :fahrenheit ""})]
+    (fn []
+      [:div
+       [:h1 "Temperature Converter"]
+       [:input {:type "number"
+                :on-input #(reset! temperature (let [val (-> % .-target .-value)]
+                                                 {:celsius val :fahrenheit (.round js/Math (celsius->fahrenheit val))}))
+                :value (:celsius @temperature)}]
+       [:input {:type "number"
+                :on-input #(reset! temperature (let [val (-> % .-target .-value)]
+                                                 {:fahrenheit val :celsius (.round js/Math (fahrenheit->celsius val))}))
+                :value (:fahrenheit @temperature)}]])))
 
 
 
-;;  [:input {:name "Fahrenheit" :value (:fahrenheit @temperature-data) :type "number" :placeholder "Enter temperature…" 
-;;             :on-change {#(reset! value (-> % .-target .-value))}}]
 
 
-;; (ns example
-;;   (:require [reagent.core :as r]))
-;; (defn atom-input [value]
-;;   [:input {:type "text"
-;;            :value @value
-;;            :on-change #(reset! value (-> % .-target .-value))}])
+  ; Originally I was hoping to write a single function i could use to alter state according to which input has changed — it seemed elegant as an idea. this wasn't a good solution for several reasons:
+  ;; (defn calc-temp [deg unit]
+  ;;   (cond-> 0
+  ;;     (= unit "fahrenheit") (* (- deg 32) (/ 5 9))
+  ;;     (= unit "celsius") (+ 32 (* deg (/ 9 5)))))
 
-;; (defn shared-state []
-;;   (let [val (r/atom "foo")]
-;;     (fn []
-;;       [:div
-;;        [:p "The value is now: " @val]
-;;        [:p "Change it here: " [atom-input val]]])))
+  ;; (defn input-element
+  ;;   "A generic input element which updates its value on change"
+  ;;   [id name type value]
+  ;;   [:input {:id id
+  ;;            :name name
+  ;;            :class "form-control"
+  ;;            :type type
+  ;;            :defaultValue @value
+  ;;            :on-change #(swap! temperature-data (keyword type) (calc-temp (-> % .-target .-value) @type))}])
+  ;; Used swap! beacuse intuitively I guess that sounds like it's for
+  ;; transient changes vs !reset for, uh, resetting state
+
+
+  ;; (defn fahrenheit-input
+  ;;   [deg]
+  ;;   (input-element "fahrenheit" "fahrenheit" "number" deg))
+
+  ;; (defn celsius-input
+  ;;   [deg]
+  ;;   (input-element "celsius" "celsius" "number" deg))
+  ;;   
+  ;; (defn component []
+  ;;   (let [temp (atom {:c "" :f ""})]
+  ;;     (fn []
+  ;;       [:div
+;;       (let [f (atom 0)])
+;;       ;;  (fahrenheit-input f))
+        ;; (let [c (atom 0)])
+        ;;  (celsius-input c))])
